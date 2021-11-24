c4ac2c292d2bce353b4f904ffbad133b6ce6db14
Aravind M <aravind/mahedevan4@gmail.com>
2021-11-24 19:01:06 +0530

    Fixed name and Title of Prasad Varma

--- pages/index.js
+++ pages/index.js
@@ -350,8 +350,8 @@ export default function Home() {
                   imgSrc="people/instructors/Srikanth.png"
                 />
                 <ImageWithDescription
-                  name="Pramod Verma"
-                  caption={<>Chief Architect, India Stack</>}
+                  name="Pramod Varma"
+                  caption={<>Chief Architect, Aadhaar & IndiaStack</>}
                   imgSrc="people/instructors/Pramod-Verma.png"
                 />
                 <ImageWithDescription
